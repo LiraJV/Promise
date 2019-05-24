9702bc215c74c9aaf43684da61282a43dde87905
JOAO VITOR DA SILVA LIRA <543707362@alunos.etec.itaqua>
2019-05-23 22:30:35 -0300

    aprendemos promise

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private alert:AlertController){}
 
+  async exibirAlerta(){
+    let temp =  await this.alert.create({
+       header: 'Alerta para o 2°Ds',
+       subHeader: '2° Ds merece MB?',
+       buttons: [{
+         text: 'Não', 
+         handler: function(){
+           console.log("Não merecem.")
+         }
+       }, {
+         text: 'Sim',
+         handler: function(){
+           console.log("Merecem sim")
+         }
+       }]
+    })
+
+    await temp.present()
+  }
 }
